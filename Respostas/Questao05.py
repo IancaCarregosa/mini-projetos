8052a1f74c71d5bd032d30c91e6bb0cffbb3d29f
IancaCarregosa <Iancacarregosa@gmail.com>
2024-10-24 23:48:44 -0300

    questoes de lógicas

---
+++ Respostas/Questao05.py
@@ -0,0 +1,4 @@
+# Para descobrir isso eu ligo o primeiro interruptor e mantenho ligado por cerca de dez a quinze minutos e o desligo, depois eu ligo o segundo interruptor e vou para as salas com as luzes.
+# A sala onde a luz estiver desligada, porém quente, é a sala do primeiro interruptor, pois não deu tempo da lampada esfriar.
+# A sala que estiver com luz ligada é aquela do segundo interruptor.
+# A sala que estiver com a luz totalmente desligada seria o ultimo interruptor que não toquei.
